f146923be9f8c702e98d3cb9b9fbe50d844daf61
Jake Gadaleta <me@jakegads.dev>
2023-11-06 17:57:44 -0500

    hardsize

--- src/components/Head.js
+++ src/components/Head.js
@@ -112,7 +112,7 @@ export default function Head() {
             <div key={missionIndex}>
               <h2>{mission.name}</h2>
               <p>{mission.helpText}</p>
-              {mission.photo && <img src={mission.photo} alt={mission.name} />}
+              {mission.photo && <img src={mission.photo} alt={mission.name} width="300" height="300"/>}
               <List>
                 {mission.options.map((option, optionIndex) => (
                   <ListItem key={optionIndex}>
